d2847d978214c1d107392824974e3a4a48fea7eb
Pedro Menzinger <105810911+pedromzn@users.noreply.github.com>
2024-05-22 11:12:52 -0300

    Add files via upload

--- Cusos/index.js
+++ Cusos/index.js
@@ -1,7 +1,46 @@
+//é possivel otimizar essa rotina em 1000% porém meu tempo é minino então se caso você conhecer um pouco de js tente otimizar esse codigo
+
+
+
+
+// variaveis do dom
+const links_1 = document.querySelector('.esconde-1');
+const links_2 = document.querySelector('.esconde-2');
+const links_3 = document.querySelector('.esconde-3');
+const links_4 = document.querySelector('.esconde-4');
+const links_5 = document.querySelector('.esconde-5');
+const links_6 = document.querySelector('.esconde-6');
+const links_7 = document.querySelector('.esconde-7');
+const links_8 = document.querySelector('.esconde-8');
+const links_9 = document.querySelector('.esconde-9');
+const links_10 = document.querySelector('.esconde-10');
+//RH
+const BoxRH = document.getElementById('RH-box');
 const seta_Rh = document.getElementById('RH');
-const links = document.querySelector('.links');
-const Box = document.getElementById('RH-box')
+//ESG
+const seta_ESG = document.getElementById('ESG');
+const BoxESG = document.getElementById('ESG-box');
+//PCA
+const seta_BPO = document.getElementById('BPO');
+const BoxBPO = document.getElementById('BPO-box');
+
 seta_Rh.addEventListener('click', () =>{
-    links.classList.toggle('active');
-    Box.classList.toggle('active');
+    mudaEstilo(links_1,BoxRH);
+});
+
+seta_ESG.addEventListener('click', () =>{
+    mudaEstilo(links_2, BoxESG);
 })
+
+seta_BPO.addEventListener('click', () =>{
+    mudaEstilo(links_3, BoxBPO);
+})
+
+function mudaEstilo(link, box){
+    link.classList.toggle('active');
+    box.classList.toggle('active');
+};
+
+// function LimpaEstilo(link, link2, link3){
+//     link.style.display = 'none';
+// }
